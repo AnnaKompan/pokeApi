a34bb48c5d99e0430ce9ce2fe3a8c55e45247809
Anna Kompan <annakompanch@gmail.com>
2025-11-24 22:45:31 +0800

    added loading and loadmore

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,8 +1,12 @@
 const BASE_URL = `https://pokeapi.co/api/v2`;
 
-function fetchPokemon(pokemonId) {
-  const url = `${BASE_URL}/pokemon/${pokemonId}`;
-  return fetch(url).then(response => response.json());
-}
-
-export default { fetchPokemon };
+export default {
+  fetchPokemon(nameID) {
+    return fetch(`${BASE_URL}/pokemon/${nameID}`).then(response =>
+      response.json()
+    );
+  },
+  fetchAll(limit = 150) {
+    return fetch(`${BASE_URL}/pokemon?limit=${limit}`).then(res => res.json());
+  },
+};
--- src/js/fetchApi.js
+++ src/js/fetchApi.js
@@ -6,15 +6,51 @@ const refs = {
   searchForm: document.querySelector('.poke_form'),
 };
 
+const moreBtn = document.querySelector('.more_btn');
+moreBtn.addEventListener('click', morePokemons);
+
+document.addEventListener('DOMContentLoaded', loadAll);
 refs.searchForm.addEventListener('submit', onSearch);
+
+// function loadAll() {
+//   API.fetchAll(20)
+//     .then(data => {
+//       return Promise.all(
+//         data.results.map(p => fetch(p.url).then(res => res.json()))
+//       );
+//     })
+//     .then(renderPokemonList)
+//     .catch(console.error);
+// }
+function loadAll() {
+  fetchBatch();
+}
+let limit = 20;
+let offset = 0;
+let totalCount = 0;
+function fetchBatch() {
+  API.fetchAll(limit, offset)
+    .then(data => {
+      totalCount = data.count;
+      offset += limit;
+
+      return Promise.all(
+        data.results.map(p => fetch(p.url).then(res => res.json()))
+      );
+    })
+    .then(renderPokemonList)
+    .catch(err => console.error(err));
+}
+
+function renderPokemonList(pokemonArray) {
+  pokemonArray.forEach(pokemonCardTpl);
+}
 
 function onSearch(e) {
   e.preventDefault();
 
   const form = e.currentTarget;
-  console.log(form.elements);
   const searchQuery = form.elements.query.value.trim();
-  console.log(searchQuery);
 
   API.fetchPokemon(searchQuery)
     .then(pokemonCardTpl)
@@ -24,4 +60,12 @@ function onSearch(e) {
 
 function onFetchErr() {
   alert('алярм алярм!!!  USE NUMBERS!!!');
+}
+
+function morePokemons() {
+  if (offset >= totalCount) {
+    alert('No More Pokemons to load!');
+    return;
+  }
+  fetchBatch();
 }
