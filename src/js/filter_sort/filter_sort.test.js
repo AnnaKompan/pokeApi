71093a31c86b626d575281201f49a0a9e0dc9035
Anna Kompan <annakompanch@gmail.com>
2025-11-25 15:53:54 +0800

    old_v_test

--- src/js/filter_sort/filter_sort.test.js
+++ src/js/filter_sort/filter_sort.test.js
@@ -54,74 +54,74 @@ describe('Sorting pokemons', () => {
   });
 });
 
-import pokemonCardTpl from '../partials/markup';
-import API from '../js/api-service';
+// import pokemonCardTpl from '../partials/markup';
+// import API from '../js/api-service';
 
-const refs = {
-  cardContainer: document.querySelector('.card-container'),
-  searchForm: document.querySelector('.poke_form'),
-};
+// const refs = {
+//   cardContainer: document.querySelector('.card-container'),
+//   searchForm: document.querySelector('.poke_form'),
+// };
 
-const moreBtn = document.querySelector('.more_btn');
-moreBtn.addEventListener('click', morePokemons);
+// const moreBtn = document.querySelector('.more_btn');
+// moreBtn.addEventListener('click', morePokemons);
 
-document.addEventListener('DOMContentLoaded', loadAll);
-refs.searchForm.addEventListener('submit', onSearch);
+// document.addEventListener('DOMContentLoaded', loadAll);
+// refs.searchForm.addEventListener('submit', onSearch);
 
-function loadAll() {
-  API.fetchAll(20)
-    .then(data => {
-      return Promise.all(
-        data.results.map(p => fetch(p.url).then(res => res.json()))
-      );
-    })
-    .then(renderPokemonList)
-    .catch(console.error);
-}
-function loadAll() {
-  fetchBatch();
-}
-let limit = 20;
-let offset = 0;
-let totalCount = 0;
-function fetchBatch() {
-  API.fetchAll(limit, offset)
-    .then(data => {
-      totalCount = data.count;
-      offset += limit;
+// function loadAll() {
+//   API.fetchAll(20)
+//     .then(data => {
+//       return Promise.all(
+//         data.results.map(p => fetch(p.url).then(res => res.json()))
+//       );
+//     })
+//     .then(renderPokemonList)
+//     .catch(console.error);
+// }
+// function loadAll() {
+//   fetchBatch();
+// }
+// let limit = 20;
+// let offset = 0;
+// let totalCount = 0;
+// function fetchBatch() {
+//   API.fetchAll(limit, offset)
+//     .then(data => {
+//       totalCount = data.count;
+//       offset += limit;
 
-      return Promise.all(
-        data.results.map(p => fetch(p.url).then(res => res.json()))
-      );
-    })
-    .then(renderPokemonList)
-    .catch(err => console.error(err));
-}
+//       return Promise.all(
+//         data.results.map(p => fetch(p.url).then(res => res.json()))
+//       );
+//     })
+//     .then(renderPokemonList)
+//     .catch(err => console.error(err));
+// }
 
-function renderPokemonList(pokemonArray) {
-  pokemonArray.forEach(pokemonCardTpl);
-}
+// function renderPokemonList(pokemonArray) {
+//   pokemonArray.forEach(pokemonCardTpl);
+// }
 
-function onSearch(e) {
-  e.preventDefault();
+// function onSearch(e) {
+//   e.preventDefault();
 
-  const form = e.currentTarget;
-  const searchQuery = form.elements.query.value.trim();
+//   const form = e.currentTarget;
+//   const searchQuery = form.elements.query.value.trim();
 
-  API.fetchPokemon(searchQuery)
-    .then(pokemonCardTpl)
-    .catch(onFetchErr)
-    .finally(() => form.reset());
-}
+//   API.fetchPokemon(searchQuery)
+//     .then(pokemonCardTpl)
+//     .catch(onFetchErr)
+//     .finally(() => form.reset());
+// }
 
-function onFetchErr() {
-  alert('алярм алярм!!!  USE NUMBERS!!!');
-}
+// function onFetchErr() {
+//   alert('алярм алярм!!!  USE NUMBERS!!!');
+// }
 
-function morePokemons() {
-  if (offset >= totalCount) {
-    alert('No More Pokemons to load!');
-    return;
-  }
-  fetchBatch();
-}
+// function morePokemons() {
+//   if (offset >= totalCount) {
+//     alert('No More Pokemons to load!');
+//     return;
+//   }
+//   fetchBatch();
+// }
